d874661f1842a781f69d498ff390ae1d7781ee87
XxRexRaptorxX <Raptor-Simon@web.de>
2023-11-06 19:25:32 +0100

    additional item tags added + trimmable armor

--- src/main/java/xxrexraptorxx/extragems/datagen/DataGenerators.java
+++ src/main/java/xxrexraptorxx/extragems/datagen/DataGenerators.java
@@ -5,6 +5,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,10 +23,11 @@ public class DataGenerators {
         PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper helper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         BlockTagGen blockTags = new BlockTagGen(packOutput, lookupProvider, event.getExistingFileHelper());
 
         generator.addProvider(event.includeServer(), blockTags);
-        //generator.addProvider(event.includeServer(), new ItemTagGen(packOutput, lookupProvider, blockTags, event.getExistingFileHelper())); TODO
+        generator.addProvider(event.includeServer(), new ItemTagGen(packOutput, lookupProvider, blockTags.contentsGetter(), existingFileHelper));
 
         generator.addProvider(event.includeClient(), new BlockStateGen(packOutput, helper));
         generator.addProvider(event.includeClient(), new ItemModelGen(packOutput, helper));
--- src/main/java/xxrexraptorxx/extragems/datagen/ItemTagGen.java
+++ src/main/java/xxrexraptorxx/extragems/datagen/ItemTagGen.java
@@ -34,7 +34,79 @@ public class ItemTagGen extends ItemTagsProvider {
                         ModItems.CHARGED_CRYSTAL.get(),
                         ModItems.CHARGED_DIAMOND.get(),
                         ModItems.CHARGED_EMERALD.get()
-                        );
+                );
+
+        tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(   ModItems.AMETHYST_HELMET.get(),
+                        ModItems.RUBY_HELMET.get(),
+                        ModItems.SAPPHIRE_HELMET.get(),
+                        ModItems.TOPAZ_HELMET.get(),
+                        ModItems.CRYSTAL_HELMET.get(),
+                        ModItems.EMERALD_HELMET.get(),
+                        ModItems.AMETHYST_CHESTPLATE.get(),
+                        ModItems.RUBY_CHESTPLATE.get(),
+                        ModItems.SAPPHIRE_CHESTPLATE.get(),
+                        ModItems.TOPAZ_CHESTPLATE.get(),
+                        ModItems.CRYSTAL_CHESTPLATE.get(),
+                        ModItems.EMERALD_CHESTPLATE.get(),
+                        ModItems.AMETHYST_LEGGINGS.get(),
+                        ModItems.RUBY_LEGGINGS.get(),
+                        ModItems.SAPPHIRE_LEGGINGS.get(),
+                        ModItems.TOPAZ_LEGGINGS.get(),
+                        ModItems.CRYSTAL_LEGGINGS.get(),
+                        ModItems.EMERALD_LEGGINGS.get(),
+                        ModItems.AMETHYST_BOOTS.get(),
+                        ModItems.RUBY_BOOTS.get(),
+                        ModItems.SAPPHIRE_BOOTS.get(),
+                        ModItems.TOPAZ_BOOTS.get(),
+                        ModItems.CRYSTAL_BOOTS.get(),
+                        ModItems.EMERALD_BOOTS.get()
+                );
+
+        tag(ItemTags.PICKAXES)
+                .add(   ModItems.AMETHYST_PICKAXE.get(),
+                        ModItems.RUBY_PICKAXE.get(),
+                        ModItems.SAPPHIRE_PICKAXE.get(),
+                        ModItems.TOPAZ_PICKAXE.get(),
+                        ModItems.CRYSTAL_PICKAXE.get(),
+                        ModItems.EMERALD_PICKAXE.get()
+                );
+
+        tag(ItemTags.SWORDS)
+                .add(   ModItems.AMETHYST_SWORD.get(),
+                        ModItems.RUBY_SWORD.get(),
+                        ModItems.SAPPHIRE_SWORD.get(),
+                        ModItems.TOPAZ_SWORD.get(),
+                        ModItems.CRYSTAL_SWORD.get(),
+                        ModItems.EMERALD_SWORD.get()
+                );
+
+        tag(ItemTags.AXES)
+                .add(   ModItems.AMETHYST_AXE.get(),
+                        ModItems.RUBY_AXE.get(),
+                        ModItems.SAPPHIRE_AXE.get(),
+                        ModItems.TOPAZ_AXE.get(),
+                        ModItems.CRYSTAL_AXE.get(),
+                        ModItems.EMERALD_AXE.get()
+                );
+
+        tag(ItemTags.SHOVELS)
+                .add(   ModItems.AMETHYST_SHOVEL.get(),
+                        ModItems.RUBY_SHOVEL.get(),
+                        ModItems.SAPPHIRE_SHOVEL.get(),
+                        ModItems.TOPAZ_SHOVEL.get(),
+                        ModItems.CRYSTAL_SHOVEL.get(),
+                        ModItems.EMERALD_SHOVEL.get()
+                );
+
+        tag(ItemTags.HOES)
+                .add(   ModItems.AMETHYST_HOE.get(),
+                        ModItems.RUBY_HOE.get(),
+                        ModItems.SAPPHIRE_HOE.get(),
+                        ModItems.TOPAZ_HOE.get(),
+                        ModItems.CRYSTAL_HOE.get(),
+                        ModItems.EMERALD_HOE.get()
+                );
     }
 
 }
