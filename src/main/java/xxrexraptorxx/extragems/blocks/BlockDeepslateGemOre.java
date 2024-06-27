ae5c62ced9cd3774e7f08a5a9c7d07ddf8dcee32
XxRexRaptorxX <Raptor-Simon@web.de>
2024-06-27 23:09:11 +0200

    update to MC 1.21

--- src/main/java/xxrexraptorxx/extragems/blocks/BlockDeepslateGemOre.java
+++ src/main/java/xxrexraptorxx/extragems/blocks/BlockDeepslateGemOre.java
@@ -2,12 +2,18 @@ package xxrexraptorxx.extragems.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -26,8 +32,8 @@ public class BlockDeepslateGemOre extends Block {
 	}
 
 	@Override
-	public int getExpDrop(BlockState state, LevelReader world, RandomSource randomSource, BlockPos pos) {
-		return 4 + randomSource.nextInt(4);
+	public int getExpDrop(BlockState state, net.minecraft.world.level.LevelAccessor level, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity breaker, ItemStack tool) {
+		return UniformInt.of(3, 7).sample(level.getRandom());
 	}
 
 }
