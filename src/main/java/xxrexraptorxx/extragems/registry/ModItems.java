dbc0245ea06adb0b9966e461872efef521833e27
XxRexRaptorxX <Raptor-Simon@web.de>
2024-06-29 00:39:17 +0200

    tool material values change

--- src/main/java/xxrexraptorxx/extragems/registry/ModItems.java
+++ src/main/java/xxrexraptorxx/extragems/registry/ModItems.java
@@ -41,70 +41,70 @@ public class ModItems {
 
 
     public static final DeferredItem<SwordItem> AMETHYST_SWORD = ITEMS.register("amethyst_sword", () ->
-            new SwordItem(ToolMaterials.AMETHYST,  new Item.Properties()));
+            new SwordItem(ToolMaterials.AMETHYST,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.AMETHYST, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> AMETHYST_PICKAXE = ITEMS.register("amethyst_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.AMETHYST,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.AMETHYST,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.AMETHYST, 1, -2.8f))));
     public static final DeferredItem<AxeItem> AMETHYST_AXE = ITEMS.register("amethyst_axe",
-            () -> new AxeItem(ToolMaterials.AMETHYST,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.AMETHYST,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.AMETHYST, 1.5f, -3.0f))));
     public static final DeferredItem<ShovelItem> AMETHYST_SHOVEL = ITEMS.register("amethyst_shovel",
-            () -> new ShovelItem(ToolMaterials.AMETHYST, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.AMETHYST, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.AMETHYST, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> AMETHYST_HOE = ITEMS.register("amethyst_hoe",
-            () -> new HoeItem(ToolMaterials.AMETHYST, new Item.Properties()));
+            () -> new HoeItem(ToolMaterials.AMETHYST, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.AMETHYST, 1, -0.0f))));
 
     public static final DeferredItem<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
-            new SwordItem(ToolMaterials.RUBY,  new Item.Properties()));
+            new SwordItem(ToolMaterials.RUBY,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.RUBY, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.RUBY,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.RUBY,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.RUBY, 1, -2.8f))));
     public static final DeferredItem<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe",
-            () -> new AxeItem(ToolMaterials.RUBY,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.RUBY,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.RUBY, 6, -3.1f))));
     public static final DeferredItem<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel",
-            () -> new ShovelItem(ToolMaterials.RUBY, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.RUBY, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.RUBY, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe",
-            () -> new HoeItem(ToolMaterials.RUBY, new Item.Properties()));
+            () -> new HoeItem(ToolMaterials.RUBY, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.RUBY, 1, -0.0f))));
 
     public static final DeferredItem<SwordItem> SAPPHIRE_SWORD = ITEMS.register("sapphire_sword", () ->
-            new SwordItem(ToolMaterials.SAPPHIRE,  new Item.Properties()));
+            new SwordItem(ToolMaterials.SAPPHIRE,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.SAPPHIRE, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> SAPPHIRE_PICKAXE = ITEMS.register("sapphire_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.SAPPHIRE,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.SAPPHIRE,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.SAPPHIRE, 1, -2.8f))));
     public static final DeferredItem<AxeItem> SAPPHIRE_AXE = ITEMS.register("sapphire_axe",
-            () -> new AxeItem(ToolMaterials.SAPPHIRE,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.SAPPHIRE,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.SAPPHIRE, 6, -3.1f))));
     public static final DeferredItem<ShovelItem> SAPPHIRE_SHOVEL = ITEMS.register("sapphire_shovel",
-            () -> new ShovelItem(ToolMaterials.SAPPHIRE, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.SAPPHIRE, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.SAPPHIRE, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> SAPPHIRE_HOE = ITEMS.register("sapphire_hoe",
-            () -> new HoeItem(ToolMaterials.SAPPHIRE, new Item.Properties()));
+            () -> new HoeItem(ToolMaterials.SAPPHIRE, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.SAPPHIRE, 1, -0.0f))));
 
     public static final DeferredItem<SwordItem> TOPAZ_SWORD = ITEMS.register("topaz_sword", () ->
-            new SwordItem(ToolMaterials.TOPAZ,  new Item.Properties()));
+            new SwordItem(ToolMaterials.TOPAZ,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.TOPAZ, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> TOPAZ_PICKAXE = ITEMS.register("topaz_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.TOPAZ,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.TOPAZ,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.TOPAZ, 1, -2.8f))));
     public static final DeferredItem<AxeItem> TOPAZ_AXE = ITEMS.register("topaz_axe",
-            () -> new AxeItem(ToolMaterials.TOPAZ,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.TOPAZ,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.TOPAZ, 6, -3.1f))));
     public static final DeferredItem<ShovelItem> TOPAZ_SHOVEL = ITEMS.register("topaz_shovel",
-            () -> new ShovelItem(ToolMaterials.TOPAZ, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.TOPAZ, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.TOPAZ, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> TOPAZ_HOE = ITEMS.register("topaz_hoe",
-            () -> new HoeItem(ToolMaterials.TOPAZ, new Item.Properties()));
+            () -> new HoeItem(ToolMaterials.TOPAZ, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.TOPAZ, 1, -0.0f))));
 
     public static final DeferredItem<SwordItem> CRYSTAL_SWORD = ITEMS.register("crystal_sword", () ->
-            new SwordItem(ToolMaterials.CRYSTAL,  new Item.Properties()));
+            new SwordItem(ToolMaterials.CRYSTAL,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.CRYSTAL, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> CRYSTAL_PICKAXE = ITEMS.register("crystal_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.CRYSTAL,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.CRYSTAL,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.CRYSTAL, 1, -2.8f))));
     public static final DeferredItem<AxeItem> CRYSTAL_AXE = ITEMS.register("crystal_axe",
-            () -> new AxeItem(ToolMaterials.CRYSTAL,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.CRYSTAL,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.CRYSTAL, 6, -3.1f))));
     public static final DeferredItem<ShovelItem> CRYSTAL_SHOVEL = ITEMS.register("crystal_shovel",
-            () -> new ShovelItem(ToolMaterials.CRYSTAL, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.CRYSTAL, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.CRYSTAL, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> CRYSTAL_HOE = ITEMS.register("crystal_hoe",
-            () -> new HoeItem(ToolMaterials.CRYSTAL, new Item.Properties()){});
+            () -> new HoeItem(ToolMaterials.CRYSTAL, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.CRYSTAL, 1, -0.0f))));
 
     public static final DeferredItem<SwordItem> EMERALD_SWORD = ITEMS.register("emerald_sword", () ->
-            new SwordItem(ToolMaterials.EMERALD,  new Item.Properties()));
+            new SwordItem(ToolMaterials.EMERALD,  new Item.Properties().attributes(SwordItem.createAttributes(ToolMaterials.EMERALD, 3, -2.4f))));
     public static final DeferredItem<PickaxeItem> EMERALD_PICKAXE = ITEMS.register("emerald_pickaxe",
-            () -> new PickaxeItem(ToolMaterials.EMERALD,  new Item.Properties()));
+            () -> new PickaxeItem(ToolMaterials.EMERALD,  new Item.Properties().attributes(PickaxeItem.createAttributes(ToolMaterials.EMERALD, 1, -2.8f))));
     public static final DeferredItem<AxeItem> EMERALD_AXE = ITEMS.register("emerald_axe",
-            () -> new AxeItem(ToolMaterials.EMERALD,  new Item.Properties()));
+            () -> new AxeItem(ToolMaterials.EMERALD,  new Item.Properties().attributes(AxeItem.createAttributes(ToolMaterials.EMERALD, 6, -3.1f))));
     public static final DeferredItem<ShovelItem> EMERALD_SHOVEL = ITEMS.register("emerald_shovel",
-            () -> new ShovelItem(ToolMaterials.EMERALD, new Item.Properties()));
+            () -> new ShovelItem(ToolMaterials.EMERALD, new Item.Properties().attributes(ShovelItem.createAttributes(ToolMaterials.EMERALD, 1.5f, -3.0f))));
     public static final DeferredItem<HoeItem> EMERALD_HOE = ITEMS.register("emerald_hoe",
-            () -> new HoeItem(ToolMaterials.EMERALD, new Item.Properties()));
+            () -> new HoeItem(ToolMaterials.EMERALD, new Item.Properties().attributes(HoeItem.createAttributes(ToolMaterials.EMERALD, 1, -0.0f))));
 
     public static final DeferredItem<ArmorItem> AMETHYST_HELMET = ITEMS.register("amethyst_helmet", () -> new ArmorItem(ModArmorMaterials.AMETHYST_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final DeferredItem<ArmorItem> AMETHYST_LEGGINGS = ITEMS.register("amethyst_leggings", () -> new ArmorItem(ModArmorMaterials.AMETHYST_ARMOR_MATERIAL, ArmorItem.Type.LEGGINGS, new Item.Properties()));
--- src/main/java/xxrexraptorxx/extragems/registry/ToolMaterials.java
+++ src/main/java/xxrexraptorxx/extragems/registry/ToolMaterials.java
@@ -6,11 +6,11 @@ import xxrexraptorxx.extragems.utils.ModTags;
 
 public class ToolMaterials {
 
-    public static final SimpleTier AMETHYST = new SimpleTier(ModTags.INCORRECT_FOR_AMETHYST_TAG, 500, 7.0f, 2.5f, 30, () -> Ingredient.of(ModTags.AMETHYST_TAG));
+    public static final SimpleTier AMETHYST = new SimpleTier(ModTags.INCORRECT_FOR_AMETHYST_TAG, 550, 7.0f, 2.5f, 30, () -> Ingredient.of(ModTags.AMETHYST_TAG));
     public static final SimpleTier RUBY = new SimpleTier(ModTags.INCORRECT_FOR_RUBY_TAG, 500, 8.0f, 2.0f, 15, () -> Ingredient.of(ModTags.RUBY_TAG));
     public static final SimpleTier SAPPHIRE = new SimpleTier(ModTags.INCORRECT_FOR_SAPPHIRE_TAG, 500, 7.0f, 3.0f, 15, () -> Ingredient.of(ModTags.SAPPHIRE_TAG));
     public static final SimpleTier TOPAZ = new SimpleTier(ModTags.INCORRECT_FOR_CRYSTAL_TAG, 450, 7.5f, 3.5f, 12, () -> Ingredient.of(ModTags.CRYSTAL_TAG));
     public static final SimpleTier CRYSTAL = new SimpleTier(ModTags.INCORRECT_FOR_TOPAZ_TAG, 450, 8.0f, 3.0f, 20, () -> Ingredient.of(ModTags.TOPAZ_TAG));
-    public static final SimpleTier EMERALD = new SimpleTier(ModTags.INCORRECT_FOR_EMERALD_TAG, 600, 7.0f, 3.0f, 25, () -> Ingredient.of(ModTags.EMERALD_TAG));
+    public static final SimpleTier EMERALD = new SimpleTier(ModTags.INCORRECT_FOR_EMERALD_TAG, 550, 7.0f, 3.0f, 25, () -> Ingredient.of(ModTags.EMERALD_TAG));
 
 }
